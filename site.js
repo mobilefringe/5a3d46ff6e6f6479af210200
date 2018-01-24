e1b626d8f958bcb827a79f3d710688b561857003
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2018-01-24 16:31:52 -0500

    Anaheim Hills 1.3  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Anaheim Hills Festival",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/57f2b1016e6f645d03550000/image/jpeg/undefined/dtl_500x500_logo.jpg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
+                    "social_feed" : "//anaheim.mallmaverick.com/api/v2/anaheim/social.json"
                 }
